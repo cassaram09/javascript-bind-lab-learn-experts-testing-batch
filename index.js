a6209ba02c63cf67b369434e397e44208a885e47
cassaram09 <cassaram09@gmail.com>
2016-12-05 14:56:36 -0500

    commit

--- index.js
+++ index.js
@@ -1,8 +1,20 @@
 const app = "I don't do much.";
 
 var digitalClock = {
-  time: new Date().getSeconds(),
-  startTicking: function(time){
-    this.time = time + 1;
-  }.bind(this)
+  time: Math.round(Date.now() / 1000),
+  startTicking: function(){
+    setInterval(function() { this.time++ }.bind(this), 1000)
+  }
+}
+
+function censor(word, string){
+  return string.replace(word, 'BLEEP');
+}
+
+function violenceCensor(){
+
+}
+
+function drugsCensor(){
+  
 }
